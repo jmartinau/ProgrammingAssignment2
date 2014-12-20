e9a38dd9a3f2d699be0b0da514ef4bed429a0673
jmartinau <jmartin@knology.net>
2014-12-20 10:36:51 -0600

    Add cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The two functions below together calculate he inverse of a matrix.  If the 
+#   inverse has already been calculated, the inverse will be returned without
+#   recalculating.
 
-## Write a short comment describing this function
+# This function creates other functions that are used when calculating/returning
+#   the inverse of a matrix.  These formulas are used in the cacheSolve function 
+#   below.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+      # Set original value of the inverse matrix to NULL
+      inv <- NULL  
+      
+      #     The set function from the original seems to have no purpose so I commented it out
+      #      set <- function(y) {  # 
+      #            z <<- y
+      #            inv <<- NULL
+      #      }
+      
+      # Store input argument so it can be retrieved by cacheSolve
+      get <- function() x
+      
+      # Create a function that stores inverted matrix in the global environment
+      setinv <- function(invrs) inv <<- invrs
+      
+      # Create a function that retrieves the value of the inverted matrix.
+      #  Used by cacheSolve to assign current value of inverse (can be NULL)
+      getinv <- function() inv
+      
+      # Create list of formulas that is returned to the global environment
+      list(get=get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+# This function determines whether a inverse has already been calculated
+# If calculated, it returns the value without doing the calculation again
+# If not calculated, it calculates the inverse and returns the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      # Retrieve the value of the inverse matrix (NULL or already calculated)
+      # Will look outside of function to global environment to find
+      inv <- x$getinv()
+      
+      # Return value of inverse if it has already been calculated
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      
+      # If the inverse has not been calculated, get matrix to invert.  will find
+      #  it in the global environment
+      data <- x$get()
+      
+      # Invert the matrix
+      inv <- solve(data, ...)
+      
+      # Execute function in global environment that sets value of inverse matrix
+      #   to the calculated value
+      x$setinv(inv)
+      
+      # Returns the value of the inverted matrix
+      inv
+      
 }
+
